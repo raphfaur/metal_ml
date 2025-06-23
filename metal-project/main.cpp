b05b0df29a1e2b5bb2b906e85b0d69ffe98e929b
Raphaël Faure <raphael.faure2@student-cs.fr>
2025-06-23 21:02:25 +0200

    Initial Commit

---
+++ metal-project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  metal-project
+//
+//  Created by Raphaël Faure on 23/06/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
